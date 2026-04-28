901dd03d379f3ec9d163be21f3e66d4d494b82d5
unknown <susanbhattarai36@gmail.com>
2026-04-28 11:48:25 +0545

    testprepration pages added

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,11 @@ import Japan from './pages/Japan'
 import Uk from './pages/Uk'
 import Usa from './pages/Usa'
 import Newzealand from './pages/Newzealand'
+import Ielts from './pages/Ielts'
+import Pte from './pages/Pte'
+import Nat from './pages/Nat'
+import Topik from './pages/Topik'
+
 
 function App() {
   return (
@@ -35,6 +40,11 @@ function App() {
             <Route path="/uk" element={<Uk />} />
             <Route path="/usa" element={<Usa />} />
             <Route path="/new-zealand" element={<Newzealand />} />
+            <Route path="/ielts" element={<Ielts />} />
+            <Route path="/pte" element={<Pte />} />
+            <Route path="/nat" element={<Nat />} />
+            <Route path="/topik" element={<Topik />} />
+
           </Routes>
         </main>
         <Footer />
